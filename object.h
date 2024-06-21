1dc0de07eb30a4f75118281db153b79b0734bda6
Xinying Yu <xinying.yu@corigine.com>
2024-06-20 22:08:15 -0400

    ci: calls and functions
    Add function objects and releated process.
    One problem left, If we run the example fib(35), the print
    infomation can not convergence.

--- object.h
+++ object.h
@@ -2,13 +2,17 @@
 #define clox_object_h
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value)       (AS_OBJ(value)->type)
 
+#define IS_FUNCTION(value)    isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value)      isObjType(value, OBJ_STRING)
 
 typedef enum {
+  OBJ_FUNCTION,
+  OBJ_NATIVE,
   OBJ_STRING,
 }ObjType;
 
@@ -17,6 +21,20 @@ struct Obj {
   struct Obj* next;
 };
 
+typedef struct {
+  Obj obj;
+  int arity;
+  Chunk chunk;
+  ObjString* name;
+} ObjFunction;
+
+typedef Value (*NativeFn)(int argCount, Value* args);
+
+typedef struct {
+  Obj obj;
+  NativeFn function;
+} ObjNative;
+
 struct ObjString {
   Obj obj;
   int length;
@@ -24,6 +42,8 @@ struct ObjString {
   uint32_t hash;
 };
 
+ObjFunction* newFunction();
+ObjNative* newNative(NativeFn function);
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
 void printObject(Value value);
@@ -32,6 +52,10 @@ static inline bool isObjType(Value value, ObjType type) {
   return IS_OBJ(value) && AS_OBJ(value)->type == type;
 }
 
+#define AS_FUNCTION(value)      ((ObjFunction*)AS_OBJ(value))
+#define AS_NATIVE(value) \
+    (((ObjNative*)AS_OBJ(value))->function)
+#define IS_NATIVE(value)       isObjType(value, OBJ_NATIVE)
 #define AS_STRING(value)        ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)       (((ObjString*)AS_OBJ(value))->chars)
 
